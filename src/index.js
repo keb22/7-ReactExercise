277ca875d97ffba59dc67d0e99a7646af3e5411f
Kevin Burgos <kevinesteban0916@gmail.com>
2023-05-17 15:51:07 -0500

    Signed-off-by: Kevin Burgos <kevinesteban0916@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import logo192 from './logo192.png'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root')
+const user= {
+  FirstName:'Kevin',
+  LastName:'Burgos',
+  Age:16,
+  School:'Promoción Social',
+  Profession:'Programmer',
+  Avatar:logo192,
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BringName=(user)=>{
+  return user.FirstName
+}
+const Saludar=(user)=>{
+  if(user){
+    return<div>
+      <img src={user.Avatar} />
+     <h1>Hola {BringName(user)}</h1>
+    </div>
+  }
+  return <h1>Hola Extraño</h1>
+}
+
+const element = Saludar(user)
+ReactDOM.render(element, container)
